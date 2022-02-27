48e437eb84cb762312d0ef627afbbbc3b8e3c874
Teerod2293 <87380445+Teerod2293@users.noreply.github.com>
2022-02-28 02:36:58 +0300

    Update farenheit.rb

--- farenheit.rb
+++ farenheit.rb
@@ -1,12 +1,16 @@
-def farenheit (gradus)
+def farenheit(gradus)
   gradus * 9 / 5 + 32
+end
+
+def string_is_number?(string, number)
+  number.to_s == string || number.to_i.to_s == string
 end
 
 p "Введите количество градусов по Цельсию:"
 user_input = STDIN.gets.chomp
 user_input_integer = user_input.to_f
 
-until user_input_integer.to_s == user_input || user_input_integer.to_i.to_s == user_input  do
+until string_is_number?(user_input, user_input_integer) do
   p "Введите число!"
   user_input = STDIN.gets.chomp
   user_input_integer = user_input.to_f
